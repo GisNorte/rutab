2da2ad6492ed84ef7df954d5d16ea6beb63ff2c9
GisNorte <30350684+GisNorte@users.noreply.github.com>
2018-07-30 12:18:13 -0400

    Add files via upload

---
+++ puntoschile.js
@@ -0,0 +1,10 @@
+var puntoschile = {
+"type": "FeatureCollection",
+"name": "puntoschile",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Punto de hidrataci�n 4" }, "geometry": { "type": "Point", "coordinates": [ -70.309540058899813, -18.344617752677159 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Punto de hidrataci�n 5" }, "geometry": { "type": "Point", "coordinates": [ -70.326937513826479, -18.408946799842798 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Nombre": "Punto de llegada" }, "geometry": { "type": "Point", "coordinates": [ -70.321816952999939, -18.477531193999937 ] } }
+]
+}
